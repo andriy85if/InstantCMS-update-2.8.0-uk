ecf4f401b7bc38f5c10ed876c7bf373d2e8247f1
Andriy <work@andriy.bokiy.name>
2017-08-03 13:09:02 +0200

    first commit

---
+++ package/system/languages/uk/controllers/files/files.php
@@ -0,0 +1,5 @@
+<?php
+
+define('LANG_FILES_CONTROLLER', 'Файли');
+define('LANG_FILES_NOT_DELETE_NO_OWNER', 'Файл не був видалений з серверу, так як ви не являєтесь його власником');
+define('LANG_FILES_NOT_DELETE_NO_PERM', 'Файл не був видалений з серверу, перевірте права доступу');
---
+++ package/system/languages/uk/controllers/messages/messages.php
@@ -0,0 +1,31 @@
+<?php
+
+    define('LANG_MESSAGES_CONTROLLER',      'Приватні повідомлення');
+    define('LANG_PM_MY_MESSAGES',           'Мої повідомлення');
+    define('LANG_PM_NO_MESSAGES',           'У вас немає особистих повідомлень');
+    define('LANG_PM_NO_NOTICES',            'Немає повідомлень');
+    define('LANG_PM_NO_ACCESS',             'Приватні повідомлення не доступні для вашої групи');
+    define('LANG_PM_SHOW_OLDER_MESSAGES',   'Показати попередні повідомлення &uarr;');
+    define('LANG_PM_ACTION_IGNORE',         'В ігнор');
+    define('LANG_PM_ACTION_FORGIVE',        'Припинити ігнор');
+    define('LANG_PM_DELETE_CONTACT',        'Видалити контакт');
+    define('LANG_PM_DELETE_CONTACT_CONFIRM','Видалити контакт зі списку?');
+    define('LANG_PM_IGNORE_CONTACT_CONFIRM','Додати контакт до чорного списку? Ви не будете отримувати від нього повідомлення після цього');
+    define('LANG_PM_SEND_TO_USER',          'Написати повідомлення');
+    define('LANG_PM_SEND_ERROR',            'Помилка відправлення повідомлення');
+    define('LANG_PM_YOU_ARE_IGNORED',       'Співрозмовник додав вас в чорний список');
+    define('LANG_PM_CONTACT_IS_IGNORED',    'Співрозмовник знаходиться в чорному списку');
+    define('LANG_PM_CONTACT_IS_PRIVATE',    'Співрозмовник приймає повідомлення тільки від своїх друзів');
+    define('LANG_PM_LIMIT',                 'Виводити повідомлень за раз');
+    define('LANG_PM_TIME_DELETE_OLD',       'Скільки зберігати видалені повідомлення?');
+    define('LANG_PM_TIME_DELETE_OLD_HINT',  '0 - зберігати завжди');
+    define('LANG_PM_REFRESH_TIME',          'Інтервал запиту про нові повідомлення');
+    define('LANG_PM_PRIVACY_CONTACT',       'Хто може писати вам особисті повідомлення?');
+    define('LANG_PM_NOTIFY_NEW',            'Сповіщати про нові повідомлення');
+    define('LANG_PM_DESKTOP_NOTIFY_NEW',    'Повідомлення від %s');
+    define('LANG_PM_USER_SEARCH',           'Почніть вводити імʼя...');
+    define('LANG_PM_IS_DELETE',             'Повідомлення видалено.');
+    define('LANG_PM_DO_RESTORE',            ' <a href="#" onclick="return icms.messages.restoreMsg(this);">Відновити</a>');
+    define('LANG_PM_CLEAR_NOTICE',          'Очистити всі сповіщення');
+    define('LANG_PM_CLEAR_NOTICE_CONFIRM',  'Очистити всі сповіщення, впевнені?');
+
